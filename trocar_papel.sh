fa989a6112ad4b2fbd630c0142e0a35e8c647cbd
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2023-03-25 17:12:57 -0300

    Update trocar_papel.sh

--- trocar_papel.sh
+++ trocar_papel.sh
@@ -15,10 +15,10 @@ cd /home/$usuario/trocar_papel
 # Caminho completo para a imagem de papel de parede
 WALLPAPER="/home/$usuario/trocar_papel/imagem.png"
 
-python3 trocar.py
-
 # Comando para definir o papel de parede
 gsettings set org.cinnamon.desktop.background picture-uri "file://${WALLPAPER}"
+
+python3 trocar.py
 
 cd ..
 
